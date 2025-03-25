0732b05a464e3909b69a8f02744653d771e38969
Ju1ceBoy <dmn170@gmail.com>
2025-03-26 02:49:30 +0300

    update import

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import styles from "./header.module.scss";
+import styles from "./Header.module.scss";
 import { useEffect, useState } from "react";
 // import style from "../../styles/container.module.scss";
 
